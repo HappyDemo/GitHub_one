bae7088f81c1b9b84a46a35b0e5e368e8a323f33
HappyDemo <18365266359@163.com>
2015-10-23 17:37:31 +0800

    helloworld
    这是我的第一个版本描述,只是一个helloworld

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitHub_one
+//
+//  Created by Caoxia on 15/10/23.
+//  Copyright © 2015年 Caoxia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
